d17df264ca14788476db31707c8988a6cdeece48
Andrew Jeffery <andrew@aj.id.au>
2021-08-20 16:44:40 +0930

    core: Add infrastructure for packet captures
    A callback and user data member is defined to abstract out the
    implementation details of capturing the packets. The captured packets
    are in DSP0236 form, i.e. no binding metadata is present.
    
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>
    Change-Id: Id8234d6e8f7856c70a0ca3fe5d4cee38ec11bd8c

--- core.c
+++ core.c
@@ -56,6 +56,10 @@ struct mctp {
 	mctp_rx_fn message_rx;
 	void *message_rx_data;
 
+	/* Packet capture callback */
+	mctp_capture_fn		capture;
+	void			*capture_data;
+
 	/* Message reassembly.
 	 * @todo: flexible context count
 	 */
@@ -295,6 +299,12 @@ void mctp_set_max_message_size(struct mctp *mctp, size_t message_size)
 	mctp->max_message_size = message_size;
 }
 
+void mctp_set_capture_handler(struct mctp *mctp, mctp_capture_fn fn, void *user)
+{
+	mctp->capture = fn;
+	mctp->capture_data = user;
+}
+
 static void mctp_bus_destroy(struct mctp_bus *bus)
 {
 	while (bus->tx_queue_head) {
@@ -528,6 +538,9 @@ void mctp_bus_rx(struct mctp_binding *binding, struct mctp_pktbuf *pkt)
 	if (mctp_pktbuf_size(pkt) <= sizeof(struct mctp_hdr))
 		goto out;
 
+	if (mctp->capture)
+		mctp->capture(pkt, mctp->capture_data);
+
 	hdr = mctp_pktbuf_hdr(pkt);
 
 	/* small optimisation: don't bother reassembly if we're going to
@@ -652,8 +665,13 @@ out:
 
 static int mctp_packet_tx(struct mctp_bus *bus, struct mctp_pktbuf *pkt)
 {
+	struct mctp *mctp = bus->binding->mctp;
+
 	if (bus->state != mctp_bus_state_tx_enabled)
 		return -1;
+
+	if (mctp->capture)
+		mctp->capture(pkt, mctp->capture_data);
 
 	return bus->binding->tx(bus->binding, pkt);
 }
