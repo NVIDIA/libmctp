06fe645cb9ccf61d95570c881c611d27b14059cc
Faizan Ali <faizana@nvidia.com>
2025-01-28 07:15:21 -0800

    vdm-util: Fix invalid memory access for query boot status
    Fix the following issue
    
    * nullptr check for respond packets before creating json format.
    
    Fixes NvBug https://nvbugspro.nvidia.com/bug/4849821
    
    Tested:
    
    Sanity test done by  involking query_boot_status via mctp-vdm-util.
    
    Signed-off-by: Faizan Ali <faizana@nvidia.com>
    
    
    (cherry picked from commit defc10a125da9900cfc5b08c80bce718155ad32e)
    
    Co-authored-by: Faizan Ali <faizana@nvidia.com>

--- vdm/nvidia/mctp-vdm-commands.c
+++ vdm/nvidia/mctp-vdm-commands.c
@@ -1184,6 +1184,13 @@ int query_boot_status_json(int fd, uint8_t tid)
 	rc = mctp_vdm_client_send_recv(tid, fd, (uint8_t *)&cmd, sizeof(cmd),
 				       (uint8_t **)&resp, &resp_len, false);
 
+	if (rc != MCTP_REQUESTER_SUCCESS) {
+		fprintf(stderr, "%s: fail to recv [rc: %d] response\n",
+			__func__, rc);
+		free(resp);
+		return rc;
+	}
+
 	struct json_object *json_obj = json_object_new_object();
 	struct json_object *query_boot_status_response =
 		json_object_new_object();
@@ -1231,9 +1238,6 @@ int query_boot_status_json(int fd, uint8_t tid)
 
 	/* free memory */
 	free(resp);
-
-	MCTP_ASSERT_RET(rc == MCTP_REQUESTER_SUCCESS, -1,
-			"%s: fail to recv [rc: %d] response\n", __func__, rc);
 
 	return 0;
 }
@@ -1512,6 +1516,13 @@ int background_copy_json(int fd, uint8_t tid, uint8_t code)
 	rc = mctp_vdm_client_send_recv(tid, fd, (uint8_t *)&cmd, sizeof(cmd),
 				       (uint8_t **)&resp, &resp_len, false);
 
+	if (rc != MCTP_REQUESTER_SUCCESS) {
+		fprintf(stderr, "%s: fail to recv [rc: %d] response\n",
+			__func__, rc);
+		free(resp);
+		return rc;
+	}
+
 	struct json_object *json_obj = json_object_new_object();
 	struct json_object *background_copy_response = json_object_new_object();
 
@@ -1545,8 +1556,6 @@ int background_copy_json(int fd, uint8_t tid, uint8_t code)
 	/* free memory */
 	free(resp);
 
-	MCTP_ASSERT_RET(rc == MCTP_REQUESTER_SUCCESS, -1,
-			"%s: fail to recv [rc: %d] response\n", __func__, rc);
 	return 0;
 }
 
@@ -1955,6 +1964,13 @@ int in_band_json(int fd, uint8_t tid, uint8_t code)
 	rc = mctp_vdm_client_send_recv(tid, fd, (uint8_t *)&cmd, sizeof(cmd),
 				       (uint8_t **)&resp, &resp_len, false);
 
+	if (rc != MCTP_REQUESTER_SUCCESS) {
+		fprintf(stderr, "%s: fail to recv [rc: %d] response\n",
+			__func__, rc);
+		free(resp);
+		return rc;
+	}
+
 	struct json_object *json_obj = json_object_new_object();
 	struct json_object *in_band_response = json_object_new_object();
 
@@ -1975,8 +1991,6 @@ int in_band_json(int fd, uint8_t tid, uint8_t code)
 	/* free memory */
 	free(resp);
 
-	MCTP_ASSERT_RET(rc == MCTP_REQUESTER_SUCCESS, -1,
-			"%s: fail to recv [rc: %d] response\n", __func__, rc);
 	return 0;
 }
 
