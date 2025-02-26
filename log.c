4352e78c463ef425b114ea7ef8764ac02b44c853
Santosh Puranik <spuranik@nvidia.com>
2025-02-26 11:05:07 +0530

    Reapply "logging: Remove Unnecessary sprintf calls"
    This reverts commit bbeb4ce3f9e3a8e14a64f4dabb607f193983410e.
    
    Fixes NVBug https://nvbugspro.nvidia.com/bug/4733005
    
    Signed-off-by: Santosh Puranik <spuranik@nvidia.com>

--- log.c
+++ log.c
@@ -138,6 +138,9 @@ void mctp_trace_common(const char *tag, const void *const payload,
 	if (!trace_enable || len == 0)
 		return;
 
+	if ((MCTP_LOG_STDIO == log_type) && (log_stdio_level < MCTP_LOG_DEBUG))
+		return;
+
 	for (i = 0; i < limit; i++)
 		ptr += sprintf(ptr, TRACE_FORMAT, ((uint8_t *)payload)[i]);
 
