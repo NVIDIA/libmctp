c9beec6df01c4cbfdbfec9ea1ae365e049ce40b3
ueliyahu <ueliyahu@nvidia.com>
2025-07-06 03:18:50 -0700

    Fix Coverity issue 6410991 - memory leak
         fix: free context to prevent leak
    
    Fixes nvbug https://
    ipp-coverity-02:8443/#/project-view/15712/10220?selectedIssue=6410991
    
    Signed-off-by: ueliyahu <ueliyahu@nvidia.com>

--- ctrld/mctp-sdbus.c
+++ ctrld/mctp-sdbus.c
@@ -1051,6 +1051,7 @@ mctp_ctrl_sdbus_create_context(sd_bus *bus, const mctp_cmdline_args_t *cmdline)
 	context->fds = calloc(MCTP_CTRL_TOTAL_FDS, sizeof(struct pollfd));
 	if (context->fds == NULL) {
 		MCTP_CTRL_ERR("Failed to allocate pollfd\n");
+		free(context);
 		return NULL;
 	}
 	context->nfds = MCTP_CTRL_TOTAL_FDS;
